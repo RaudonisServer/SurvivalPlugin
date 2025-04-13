8de49036e7432ed0252ce06cfddb8ca5ece0639a
TheAnonym3000 <186270664+TheAnonym3000@users.noreply.github.com>
2025-04-13 16:57:38 +0200

    Updated things

--- src/main/java/xyz/anonym/raudonisSurvival/RaudonisSurvival.java
+++ src/main/java/xyz/anonym/raudonisSurvival/RaudonisSurvival.java
@@ -1,18 +1,41 @@
 package xyz.anonym.raudonisSurvival;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.anonym.raudonisSurvival.command.TPACommand;
+
+import java.util.Objects;
 
 public final class RaudonisSurvival extends JavaPlugin {
 
+    public static void log(String text) {
+        // Write into the log
+        Bukkit.getConsoleSender().sendMessage(text);
+    }
+
+    public static void sleep(int sleeptime) {
+        try {
+            Thread.sleep(sleeptime);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        Bukkit.
+        log("[§6§lRaudonisSurvival§r] Registering...");
+        register();
+        log("[§6§lRaudonisSurvival§r] Done!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void register() {
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        Objects.requireNonNull(Bukkit.getPluginCommand("tpa")).setExecutor(new TPACommand());
     }
 }
--- src/main/java/xyz/anonym/raudonisSurvival/command/TPACommand.java
+++ src/main/java/xyz/anonym/raudonisSurvival/command/TPACommand.java
@@ -1,4 +1,35 @@
 package xyz.anonym.raudonisSurvival.command;
 
-public class TPACommand {
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import xyz.anonym.raudonisSurvival.RaudonisSurvival;
+
+import java.nio.Buffer;
+
+public class TPACommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        Player teleportPlayer;
+        Player argumentPlayer;
+        if (commandSender instanceof Player) {
+            teleportPlayer = (Player) commandSender;
+            argumentPlayer = Bukkit.getPlayer(strings[0]);
+        } else {
+            commandSender.sendMessage("[§6§lRaudonisSurvival§r] §4You can't execute this!");
+            return true;
+        }
+        try {
+            argumentPlayer.sendMessage("[§6§lRaudonisSurvival§r] §e" + teleportPlayer.getName() + "§r is teleporting to you!");
+            teleportPlayer.sendMessage("[§6§lRaudonisSurvival§r] Teleporting to §e" + argumentPlayer.getName() + "§r...");
+            RaudonisSurvival.sleep(1500);
+            teleportPlayer.teleport(argumentPlayer);
+            return true;
+        } catch (Exception e) {
+            teleportPlayer.sendMessage("[§6§lRaudonisSurvival§r] §4The Player is not online!");
+            return false;
+        }
+    }
 }
